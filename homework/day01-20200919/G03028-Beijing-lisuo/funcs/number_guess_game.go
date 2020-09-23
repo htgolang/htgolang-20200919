fa4a800a85343d2193e86da0576798dc6ca3c25e
root <root@suosuoli.cn>
2020-09-23 18:39:06 +0800

    a little optimization

--- homework/day01-20200919/G03028-Beijing-lisuo/funcs/number_guess_game.go
+++ homework/day01-20200919/G03028-Beijing-lisuo/funcs/number_guess_game.go
@@ -16,7 +16,7 @@ func NumGuess() {
 	var threshold int = 5
 	// using UnixNano cause the random int more random
 	rand.Seed(time.Now().UnixNano())
-	criteria := rand.Intn(100)
+	criteria := rand.Intn(100) + 1
 	// fmt.Printf("The target is: %v\n", criteria)
 	for i := 1; i <= threshold; i++ {
 		fmt.Print("input the guess[1-100]: ")
@@ -28,7 +28,7 @@ func NumGuess() {
 			threshold += 1
 			continue
 		} else if guess < 1 || guess > 100 {
-			fmt.Println("Input the number between 1 and 100.")
+			fmt.Println("Input the number between [1,100].")
 			// give one more chance
 			threshold += 1
 			continue
