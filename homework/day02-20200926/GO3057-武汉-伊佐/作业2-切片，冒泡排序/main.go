e8e776d485ab961326c9e5d7b1c9e7c413afd973
yizuo <635126524@qq.com>
2020-09-27 19:31:28 +0800

    UPDATE: yizuo 修复冒泡排序展示BUG，新增插入排序

--- homework/day02-20200926/GO3057-武汉-伊佐/作业2-切片，冒泡排序/main.go
+++ homework/day02-20200926/GO3057-武汉-伊佐/作业2-切片，冒泡排序/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	data := []int{108, 107, 105, 109, 103, 102}
-	fmt.Printf("源数据: ", data)
+	fmt.Printf("源数据: %v\n", data)
 
 	// 找到最大的数字，将它放到列表的最后一位
 	for i, v := range data {
@@ -14,7 +14,7 @@ func main() {
 			data[i], data[len(data)-1] = data[len(data)-1], data[i]
 		}
 	}
-	fmt.Printf("找出最大的数据放到最后一位: ", data)
+	fmt.Printf("找出最大的数据放到最后一位: %v \n", data)
 
 	// 找到第二大的数字，将它放到列表的倒数第二位
 	for num := len(data) - 2; num < len(data); num++ {
@@ -24,7 +24,7 @@ func main() {
 			}
 		}
 	}
-	fmt.Printf("找出最大及其第二大的数据，放置最后两位: ", data)
+	fmt.Printf("找出最大及其第二大的数据，放置最后两位: %v \n", data)
 
 	// 冒泡排序算法，从小到大排序
 	for num := 0; num < len(data); num++ {
@@ -34,6 +34,6 @@ func main() {
 			}
 		}
 	}
-	fmt.Printf("冒泡排序算法，从小到大排序: ", data)
+	fmt.Printf("冒泡排序算法，从小到大排序: %v \n", data)
 
 }
