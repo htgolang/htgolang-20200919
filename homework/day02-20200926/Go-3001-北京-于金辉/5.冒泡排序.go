821322f7b8aa060b4b53009df474c8c11c55b28c
yujinhui <yujinhui@lohogames.com>
2020-09-29 23:20:29 +0800

    算法更新

--- homework/day02-20200926/Go-3001-北京-于金辉/5.冒泡排序.go
+++ homework/day02-20200926/Go-3001-北京-于金辉/5.冒泡排序.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	numbers := []int{108, 107, 105, 109, 103, 102}
-
-	for i := 0; i < len(numbers)-1; i++ {
-		for j := 0; j < len(numbers)-1-i; j++ {
-			if numbers[j] > numbers[j+1] {
-				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+func bubbleSort(slice []int, order bool) {
+	// 冒泡排序
+	// order: true 正序, false 倒序
+	flag := false
+	for i := 0; i < len(slice)-1; i++ {
+		for j := 0; j < len(slice)-1-i; j++ {
+			if (slice[j] > slice[j+1] && order) || (slice[j] < slice[j+1] && !order) {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+				flag = true
 			}
 		}
+		// 这是针对已经排好排的切片优化
+		// flage值未变，说明没有数据进行两两交换，说明数据已经是有序状态，提前退出循环
+		if !flag {
+			break
+		}
 	}
+}
 
-	// [102 103 105 107 108 109]
+func main() {
+	numbers := []int{108, 107, 105, 109, 103, 102}
+	//numbers := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	bubbleSort(numbers, true)
 	fmt.Println(numbers)
 }
