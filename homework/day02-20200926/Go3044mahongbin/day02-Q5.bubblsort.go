5d61aababe38144ed29a89d4a570c5e6db42ee05
max27149 <1478026873@qq.com>
2020-09-30 17:31:33 +0800

    0930 optimized

--- homework/day02-20200926/Go3044mahongbin/day02-Q5.bubblsort.go
+++ homework/day02-20200926/Go3044mahongbin/day02-Q5.bubblsort.go
@@ -5,18 +5,34 @@ import (
 )
 
 func main() {
-	heights := []int{108, 107, 105, 109, 103, 102}
-	for j := 0; j < len(heights)-1; j++ {
-		fmt.Println("============================Round", j+1)
-		for i := 0; i < len(heights)-1-j; i++ {
-			fmt.Println("compare:", heights[i], heights[i+1])
+	slice00 := []int{108, 107, 105, 109, 103, 102}
 
-			if heights[i] > heights[i+1] {
-				fmt.Println("exchage:---> ", heights[i], heights[i+1])
-				heights[i], heights[i+1] = heights[i+1], heights[i]
+//	// 01 bubblesort
+//	for j := 0; j < len(slice00)-1; j++ {
+//		fmt.Println("============================Round", j+1)
+//		for i := 0; i < len(slice00)-1-j; i++ {
+//			fmt.Println("compare:", slice00[i], slice00[i+1])
+//
+//			if slice00[i] > slice00[i+1] {
+//				fmt.Println("exchage:---> ", slice00[i], slice00[i+1])
+//				slice00[i], slice00[i+1] = slice00[i+1], slice00[i]
+//			}
+//			fmt.Println("result:", slice00)
+//		}
+//		fmt.Println("Round result:", slice00)
+//	}
+
+	// 02 bubblesort optimized
+	for i:=len(slice00)-1;i>0;i--{
+		fmt.Println("+++++++++++++++++++++++++++++Round", i)
+		for j:=0;j<i;j++{
+			if slice00[j]> slice00[i]{
+				fmt.Println("exchage:---> ", slice00[j], slice00[i])
+				slice00[j],slice00[i]=slice00[i],slice00[j]
+				fmt.Println("result:", slice00)
 			}
-			fmt.Println("result:", heights)
 		}
-		fmt.Println("Round result:", heights)
+		fmt.Println("Round result:", slice00)
 	}
 }
+//https://www.runoob.com/w3cnote/bubble-sort.html
