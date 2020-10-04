95a845918deb47cb539220d51f8ed65026026128
xiaopao-my <mayu0876@163.com>
2020-10-04 15:14:13 +0800

    day02_homework

---
+++ homework/day02-20200926/Go3016-杭州-马宇/max_numer.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+)
+func main()  {
+	var test  = []int{108, 107, 105, 109, 103, 102}
+	for j:=0;j < len(test)-1;j++ {
+		for i := 0; i < len(test)-1; i++ {
+			if test[i] > test[i+1] {
+				test[i],test[i+1] = test[i+1],test[i]
+			}
+		}
+	}
+	fmt.Printf("最大的数:%v\n",test[5])
+	fmt.Printf("排序结果:%v\n",test)
+}
