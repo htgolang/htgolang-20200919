bedb8b51384d83c9887e34c8d40110ec78e69172
hongbin <hongbin.ma@mtime.com>
2020-10-09 12:06:47 +0800

    1009

--- homework/day02-20200926/Go3044mahongbin/day02-Q6.insertionSort.go
+++ homework/day02-20200926/Go3044mahongbin/day02-Q6.insertionSort.go
@@ -1,6 +1,12 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 func main (){
+
+	//6.1
 	sli :=  []int{108, 107, 105, 109, 103, 102}
 	ls := len(sli)
 	for i:=1;i<ls;i++{
@@ -16,9 +22,25 @@ func main (){
 				fmt.Println("switch i,j >>>> ",sli)
 			}
 		}
-
 		fmt.Println("Current Result ------",sli)
 
 	}
 	fmt.Println("InsertionSort result:",sli)
+
+
+	//6.2
+	sli2 := []int{102, 103, 105, 107, 108, 109}
+	num := rand.New(rand.NewSource(time.Now().UnixNano())).Int() % 10 +100
+	fmt.Println("num:", num)
+
+	for i,v := range sli2{
+		if v == num {
+			fmt.Println("num's index:",i)
+			break
+		}else{
+			if i==len(sli2)-1 {
+				fmt.Println("num's index: -1")
+			}
+		}
+	}
 }
