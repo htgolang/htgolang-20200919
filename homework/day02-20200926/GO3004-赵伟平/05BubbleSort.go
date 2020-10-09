767a57e19a55d42277610cfb9f32f3426097c6b5
Aomine <123zhaoweiping@gmail.com>
2020-10-09 16:54:57 +0800

    homework day02 赵伟平

--- homework/day02-20200926/GO3004-赵伟平/05BubbleSort.go
+++ homework/day02-20200926/GO3004-赵伟平/05BubbleSort.go
@@ -6,14 +6,9 @@ func main() {
 	nums := [...]int{108, 107, 105, 109, 103, 102}
 
 	for j := 0; j < len(nums)-1; j++ {
-		// fmt.Printf("%s,%v\n", "j", j)
 		for i := 0; i < len(nums)-1; i++ {
-			// fmt.Printf("%s, %v\n", "i", i)
-			// fmt.Println(nums[i])
 			if nums[i] > nums[i+1] {
-				// fmt.Println(nums[i], nums[i+1])
 				nums[i], nums[i+1] = nums[i+1], nums[i]
-				// fmt.Println(nums)
 			}
 		}
 	}
