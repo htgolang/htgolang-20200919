7a1ee009f1ef649fa276a067c95c6ae0bc5a70b6
hongbin <hongbin.ma@mtime.com>
2020-10-09 16:15:51 +0800

    1009部分优化

--- homework/day02-20200926/Go3044mahongbin/day02-Q2-Q4.go
+++ homework/day02-20200926/Go3044mahongbin/day02-Q2-Q4.go
@@ -1,37 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	sl := []int{108, 107, 105, 109, 103, 102}
-	tmp := sl[0]
-	tmp_idx := 0
-	for i, v := range sl {
-		if sl[i] > tmp {
-			tmp = v
-			tmp_idx = i
+	sli := []int{106, 107, 105, 109, 109, 108, 103, 102}
+	//猴子掰包谷
+	maxNum, secondNum := sli[0], sli[0]
+	for _, v := range sli {
+		if v > maxNum {
+			secondNum = maxNum
+			maxNum = v
+		} else if v == maxNum {
+			continue
+		} else if v > secondNum {
+			secondNum = v
 		}
 	}
-	fmt.Println("Q2==The max number:", tmp)
-	fmt.Println("Q3==The origen slice:", sl)
-
-	copy(sl[tmp_idx:], sl[tmp_idx+1:])
-	sl[len(sl)-1] = tmp
-	fmt.Println("Q3==The max follows the others:", sl)
-
-	tmp = sl[0]
-	tmp_idx = 0
-	for i := 1; i < len(sl)-2; i++ {
-		if sl[i] > tmp {
-			tmp = sl[i]
-		}
-	}
-	//
-	copy(sl[tmp_idx:], sl[tmp_idx+1:])
-	sl[len(sl)-2] = tmp
-	fmt.Println("Q3==The second max follows the lessers:", sl)
-
-	//
+	fmt.Println("最大数，次大数：", maxNum, secondNum)
 }
--- homework/day02-20200926/Go3044mahongbin/day02-Q5.bubblsort.go
+++ homework/day02-20200926/Go3044mahongbin/day02-Q5.bubblsort.go
@@ -7,32 +7,34 @@ import (
 func main() {
 	slice00 := []int{108, 107, 105, 109, 103, 102}
 
-//	// 01 bubblesort
-//	for j := 0; j < len(slice00)-1; j++ {
-//		fmt.Println("============================Round", j+1)
-//		for i := 0; i < len(slice00)-1-j; i++ {
-//			fmt.Println("compare:", slice00[i], slice00[i+1])
-//
-//			if slice00[i] > slice00[i+1] {
-//				fmt.Println("exchage:---> ", slice00[i], slice00[i+1])
-//				slice00[i], slice00[i+1] = slice00[i+1], slice00[i]
-//			}
-//			fmt.Println("result:", slice00)
-//		}
-//		fmt.Println("Round result:", slice00)
-//	}
+	//	// 01 bubblesort
+	//	for j := 0; j < len(slice00)-1; j++ {
+	//		fmt.Println("============================循环计数：", j+1)
+	//		for i := 0; i < len(slice00)-1-j; i++ {
+	//			fmt.Println("比较大小：", slice00[i], slice00[i+1])
+	//
+	//			if slice00[i] > slice00[i+1] {
+	//				fmt.Println("交换位置：---> ", slice00[i], slice00[i+1])
+	//				slice00[i], slice00[i+1] = slice00[i+1], slice00[i]
+	//			}
+	//			fmt.Println("换位结果：", slice00)
+	//		}
+	//		fmt.Println("循环结束，结果为:", slice00)
+	//	}
 
-	// 02 bubblesort optimized
-	for i:=len(slice00)-1;i>0;i--{
-		fmt.Println("+++++++++++++++++++++++++++++Round", i)
-		for j:=0;j<i;j++{
-			if slice00[j]> slice00[i]{
-				fmt.Println("exchage:---> ", slice00[j], slice00[i])
-				slice00[j],slice00[i]=slice00[i],slice00[j]
-				fmt.Println("result:", slice00)
+	// 02 bubblesort 优化版
+	for i := len(slice00) - 1; i > 0; i-- {
+		fmt.Println("+++++++++++++++++++++++++++++循环计数：", i)
+		for j := 0; j < i; j++ {
+			fmt.Println("比大小：", slice00[j], slice00[i])
+			if slice00[j] > slice00[i] {
+				fmt.Println("交换位置：---> ", slice00[j], slice00[i])
+				slice00[j], slice00[i] = slice00[i], slice00[j]
+				fmt.Println("换位结果：", slice00)
 			}
 		}
-		fmt.Println("Round result:", slice00)
+		fmt.Println("循环结束，结果为:", slice00)
 	}
 }
+
 //https://www.runoob.com/w3cnote/bubble-sort.html
