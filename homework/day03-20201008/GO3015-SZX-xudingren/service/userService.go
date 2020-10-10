98091b5f93ca1f483d28919377d4d26f303e6bd9
徐鼎仁 <kevinxu@easyops.cn>
2020-10-10 14:13:30 +0800

    第3周作业

--- homework/day03-20201008/GO3015-SZX-xudingren/service/userService.go
+++ homework/day03-20201008/GO3015-SZX-xudingren/service/userService.go
@@ -42,7 +42,7 @@ func (s *UserService) HasUser(userId int) (int, error) {
 	return -1, errors.New("查无此用户")
 }
 
-func (s UserService) GetUser(idx int) []model.User {
+func (s *UserService) GetUser(idx int) []model.User {
 	return []model.User{s.users[idx]}
 }
 
