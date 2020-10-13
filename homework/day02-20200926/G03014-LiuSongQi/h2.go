ee98c43d0dcc13debebd3fc987d97add66863d0b
crazylsq <crazylsq@126.com>
2020-10-13 12:05:30 +0800

    优化h2.go

--- homework/day02-20200926/G03014-LiuSongQi/h2.go
+++ homework/day02-20200926/G03014-LiuSongQi/h2.go
@@ -11,13 +11,19 @@ int切片 []int{108, 107, 105, 109, 103, 102} 找出最大的数字
 func main() {
 	var nums = []int{108, 107, 105, 109, 103, 102}
 
-	for x := 0; x < len(nums); x++ {
-		for y := x + 1; y < len(nums); y++ {
-			if nums[x] < nums[y] {
-				nums[x], nums[y] = nums[y], nums[x]
-			}
+	// for x := 0; x < len(nums); x++ {
+	// 	for y := x + 1; y < len(nums); y++ {
+	// 		if nums[x] < nums[y] {
+	// 			nums[x], nums[y] = nums[y], nums[x]
+	// 		}
+	// 	}
+	// }
+
+	for x := 0; x < len(nums)-1; x++ {
+		if nums[x] > nums[x+1] {
+			nums[x], nums[x+1] = nums[x+1], nums[x]
 		}
 	}
 
-	fmt.Println(nums[0])
+	fmt.Println(nums[len(nums)-1])
 }
