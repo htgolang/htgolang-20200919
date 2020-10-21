1fde703e8f039e1cb34e3bad2e831983cc786b1a
Xingxing <xingxingwang122@gmail.com>
2020-10-21 23:04:24 +0800

    wangxx 第一次作业

--- homework/day01-20200919/Go3030-Bj-xingxing/homework1.go
+++ homework/day01-20200919/Go3030-Bj-xingxing/homework1.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	for i := 1; i <= 9; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Printf("%d*%d=%d ", j, i, j*i)
+func main(){
+
+	for i := 1; i<=9; i++{
+		for j := 1; j<=i; j++{
+			fmt.Printf("%d*%d=%d\t",j,i,j*i)
 
 		}
-		fmt.Println("")
+		fmt.Println()
 	}
+
 }
