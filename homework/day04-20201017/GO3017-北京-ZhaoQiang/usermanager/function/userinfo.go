e148fc6ad82f965d8dad65d25dc331dc2f766876
zhaoqiang0201 <zhaoq0201@163.com>
2020-10-24 01:28:27 +0800

    day_04

--- homework/day04-20201017/GO3017-北京-ZhaoQiang/usermanager/function/userinfo.go
+++ homework/day04-20201017/GO3017-北京-ZhaoQiang/usermanager/function/userinfo.go
@@ -2,11 +2,10 @@ package function
 
 //Users user info
 /*
-	每个元素
-    ID
-    名称
-    联系方式
-    通信地址
+   ID
+   名称
+   联系方式
+   通信地址
 */
 var Users = []map[string]string{}
 
@@ -15,13 +14,13 @@ func init() {
 		"id":   "1",
 		"name": "zhao1",
 		"tel":  "152XXXXXXXX",
-		"addr": "山西省太原市",
+		"addr": "sxiii1",
 	}
 	user2 := map[string]string{
 		"id":   "2",
 		"name": "zhao2",
 		"tel":  "152XXXXXXXX2",
-		"addr": "山西省太原市2",
+		"addr": "sxyyy2",
 	}
 
 	Users = append(Users, user1, user2)
