18d1f6016e7c56a64aefede6f4b45784140708af
root <root@suosuoli.cn>
2020-11-25 21:49:38 +0800

    make a skeleton

--- homework/day09-20201121/Go3028-Beijing-lisuo/remote_file_operation/client/client.go
+++ homework/day09-20201121/Go3028-Beijing-lisuo/remote_file_operation/client/client.go
@@ -3,47 +3,55 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 var (
-	addr        = "127.0.0.1:8889"
+	addr        = "suosuoli.cn:8889"
 	defaultPath = "/tmp/"
+
+	contentLenStr = 5
 )
 
 type CommandBody struct {
 	Cmd      string `json:"cmd"`
 	FilePath string `json:"filePath"`
 	FileName string `json:"fileName"`
-	Status   int    `json:"status"`
+}
+
+type ResponseBody struct {
+	CommandBody
+	Status int `json:"status"`
 }
 
 func main() {
-	var d = make([]byte, 20)
-	for {
-		buf := bytes.NewBuffer(d)
-		conn, err := net.Dial("tcp", addr)
-		if err != nil {
-			panic(err)
-		}
-		_, errR := conn.Read(buf.Bytes())
-		if errR != nil {
-			conn.Close()
-			panic(errR)
-		}
-		fmt.Printf("From server: %s\n", buf.Bytes())
-		buf.Reset()
-
-		toSend := Input("Client send: ")
-		_, errW := conn.Write([]byte(toSend))
-		if errW != nil {
-			conn.Close()
-			panic(errW)
-		}
+	//for {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(err)
 	}
+
+	cmd := CommandBody{"ls", "/tmp/", "server.log"}
+	WriteContentLen(conn, cmd)
+	WriteCmdBody(conn, cmd)
+	resB := ReadResponseBody(conn)
+	fmt.Println("resB: ", resB)
+
+	// read cmd's result
+	var buf = make([]byte, 200)
+	_, errr := conn.Read(buf)
+	if errr != nil {
+		panic(errr)
+	}
+	fmt.Println("list: ", string(buf))
+	conn.Close()
+
+	//	}
 	fmt.Println("Disconnected.")
 }
 
@@ -52,4 +60,61 @@ func Input(s string) string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return strings.TrimSpace(scanner.Text())
+}
+
+func WriteCmdBody(c net.Conn, cmdBody CommandBody) {
+	b, _ := json.Marshal(cmdBody)
+	_, errW := c.Write(b)
+	if errW != nil {
+		c.Close()
+		panic(errW)
+	}
+
+}
+
+func ReadResponseBody(c net.Conn) ResponseBody {
+	conLen := ReadContentLen(c)
+	var d = make([]byte, conLen)
+	buf := bytes.NewBuffer(d)
+	_, errR := c.Read(buf.Bytes())
+	if errR != nil {
+		c.Close()
+		panic(errR)
+	}
+	responseBytes := buf.Bytes()
+	var response = ResponseBody{}
+	errUnmarshal := json.Unmarshal(responseBytes, &response)
+	if errUnmarshal != nil {
+		panic(errUnmarshal)
+	}
+	return response
+}
+
+func WriteContentLen(c net.Conn, cmd CommandBody) {
+	bt, err := json.Marshal(cmd)
+	if err != nil {
+		c.Close()
+		panic(err)
+	}
+	contentLen := len(string(bt))
+	lenStr := fmt.Sprintf("%05d", contentLen)
+	_, errW := c.Write([]byte(lenStr))
+	if errW != nil {
+		c.Close()
+		panic(errW)
+	}
+}
+
+func ReadContentLen(c net.Conn) int {
+	var buf = make([]byte, contentLenStr)
+	_, errRead := c.Read(buf)
+	if errRead != nil {
+		c.Close()
+		panic(errRead)
+	}
+	len, err := strconv.Atoi(string(buf))
+	if err != nil {
+		panic(err)
+	}
+	return len
 }
--- homework/day09-20201121/Go3028-Beijing-lisuo/remote_file_operation/server/server.go
+++ homework/day09-20201121/Go3028-Beijing-lisuo/remote_file_operation/server/server.go
@@ -3,22 +3,28 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-// json := `{"cmd":"", "path":"", "fileName":""}`
-// len(json)
-//
+/*
+cmds: ls, put, get, rm
+path: /tmp/xxx
+file:
+*/
 
 var (
-	addr     = "127.0.0.1:8889"
-	servPath = "/tmp/"
+	addr          = ":8889"
+	servPath      = "/tmp/"
+	contentLenStr = 5
 )
 
-type Response struct {
+type ResponseBody struct {
 	Cmd      string `json:"cmd"`
 	FilePath string `json:"filePath"`
 	FileName string `json:"fileName"`
@@ -32,29 +38,22 @@ func main() {
 	}
 	fmt.Printf("Server starts up, listenling: %s\n", addr)
 
-	var d = make([]byte, 200)
-
-	for {
-		buf := bytes.NewBuffer(d)
-		conn, err := listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-		toSend := Input("Server send: ")
-		_, errW := conn.Write([]byte(toSend))
-		if errW != nil {
-			conn.Close()
-			panic(errW)
-		}
-		_, errR := conn.Read(buf.Bytes())
-		fmt.Println("From client: ", string(buf.Bytes()))
-		buf.Reset()
-		if errR != nil {
-			conn.Close()
-			panic(errR)
-		}
+	//for {
+	conn, err := listener.Accept()
+	if err != nil {
+		panic(err)
 	}
 
+	rec := ReadCmdBody(conn)
+	fmt.Println("rec: ", rec)
+	rec.Status = 200
+	WriteContentLen(conn, rec)
+	WriteRespondBody(conn, rec)
+
+	// parse cmdBody and decide to send file or list files
+	ListFiles(conn, rec)
+
+	conn.Close()
 	fmt.Println("Server closed.")
 
 }
@@ -64,4 +63,70 @@ func Input(s string) string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return strings.TrimSpace(scanner.Text())
+}
+
+func WriteContentLen(c net.Conn, response ResponseBody) {
+	bt, err := json.Marshal(response)
+	if err != nil {
+		c.Close()
+		panic(err)
+	}
+	contentLen := len(string(bt))
+	lenStr := fmt.Sprintf("%05d", contentLen)
+	_, errW := c.Write([]byte(lenStr))
+	if errW != nil {
+		c.Close()
+		panic(errW)
+	}
+}
+
+func ReadContentLen(c net.Conn) int {
+	var buf = make([]byte, contentLenStr)
+	_, errRead := c.Read(buf)
+	if errRead != nil {
+		c.Close()
+		panic(errRead)
+	}
+	len, err := strconv.Atoi(string(buf))
+	if err != nil {
+		panic(err)
+	}
+	return len
+}
+
+func ReadCmdBody(c net.Conn) ResponseBody {
+	conLen := ReadContentLen(c)
+	var d = make([]byte, conLen)
+	buf := bytes.NewBuffer(d)
+	_, errR := c.Read(buf.Bytes())
+	if errR != nil {
+		c.Close()
+		panic(errR)
+	}
+	cmdBodyBytes := buf.Bytes()
+	var resBeforeSend = ResponseBody{}
+	errUnmarshal := json.Unmarshal(cmdBodyBytes, &resBeforeSend)
+	if errUnmarshal != nil {
+		panic(errUnmarshal)
+	}
+	return resBeforeSend
+}
+
+func WriteRespondBody(c net.Conn, resBody ResponseBody) {
+	b, errMar := json.Marshal(resBody)
+	if errMar != nil {
+		panic(errMar)
+	}
+	_, errW := c.Write(b)
+	if errW != nil {
+		c.Close()
+		panic(errW)
+	}
+
+}
+
+func ListFiles(c net.Conn, res ResponseBody) {
+	if res.Cmd == "ls" {
+		c.Write([]byte(fmt.Sprintf("file %v is here.\n", filepath.Join(res.FilePath, res.FileName))))
+	}
 }
