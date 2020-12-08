3a6a8bde8e1372854a2963bf945b3aac2d6402eb
root <root@suosuoli.cn>
2020-12-08 21:58:15 +0800

    make up day10's homework

--- homework/day10-20201128/Go3028-Beijing-lisuo/log_tool/main.go
+++ homework/day10-20201128/Go3028-Beijing-lisuo/log_tool/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"text/template"
 )
+
+type Result struct {
+	Rank   int
+	IP     string
+	Status int
+	URL    string
+}
 
 func main() {
 
@@ -19,12 +28,67 @@ func main() {
 
 	})
 
+	http.HandleFunc("/uploadPage/", func(w http.ResponseWriter, r *http.Request) {
+		tpl, err := template.New("upload").ParseFiles("./upload.html")
+		if err != nil {
+			panic(err)
+		}
+		if err := tpl.ExecuteTemplate(w, "upload.html", ""); err != nil {
+			panic(err)
+		}
+
+	})
+
 	http.HandleFunc("/upload/", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseMultipartForm(1024 * 1024)
-		f, fr, err := r.FormFile("filename")
-		fmt.Println("f: ", f)
-		fmt.Println("fr: ", fr)
-		fmt.Println("err: ", err)
+		fmt.Println("r.URL: ", r.URL)
+		// file size limited to 1G
+		r.ParseMultipartForm(1000 << 20)
+		fmt.Println("r.Form: ", r.Form)
+		fmt.Println("r.Postform: ", r.PostForm)
+		fmt.Println("r.MultipartForm: ", r.MultipartForm)
+		if fileHeaders, ok := r.MultipartForm.File[r.Form["filename"][0]]; ok {
+			for _, fileHeader := range fileHeaders {
+				fmt.Println("fileHeader.Filename: ", fileHeader.Filename)
+				fmt.Println("fileHeader.Size: ", fileHeader.Size)
+				newFile, err := os.Create("/tmp/" + fileHeader.Filename)
+				if err != nil {
+					panic(err)
+				}
+				file, err := fileHeader.Open()
+				if err != nil {
+					panic(err)
+				}
+				io.Copy(newFile, file)
+			}
+		}
+		http.Redirect(w, r, "/", 302)
+
+	})
+
+	http.HandleFunc("/resultPage/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("resultPage==========")
+		tpl, err := template.New("result").ParseFiles("./result.html")
+		if err != nil {
+			panic(err)
+		}
+		if err := tpl.ExecuteTemplate(w, "result.html", ""); err != nil {
+			panic(err)
+		}
+		fmt.Println("resultPage==========end")
+	})
+
+	http.HandleFunc("/display/", func(w http.ResponseWriter, r *http.Request) {
+		tpl, err := template.New("display").ParseFiles("./display.html")
+		if err != nil {
+			panic(err)
+		}
+		resultList := []Result{
+			Result{1, "192.178.20.30", 500, "/"},
+			Result{2, "172.12.50.35", 200, "/cake/bake"},
+		}
+		if err := tpl.ExecuteTemplate(w, "display.html", resultList); err != nil {
+			panic(err)
+		}
 
 	})
 
