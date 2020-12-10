1730c81f9c5603b6f6ee65272afec6b63cf7738f
151813262@qq.com <151813262@qq.com>
2020-12-11 02:56:56 +0800

    fileserver

---
+++ homework/day11-20201205/GO3035-beijing-songyunfei/fileserver/log_analysis.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: log_analysis
+-- ------------------------------------------------------
+-- Server version	5.7.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fileinfo`
+--
+
+DROP TABLE IF EXISTS `fileinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fileinfo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(32) NOT NULL DEFAULT '',
+  `size` bigint(20) NOT NULL,
+  `upload_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recorder`
+--
+
+DROP TABLE IF EXISTS `recorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recorder` (
+  `fileid` bigint(20) NOT NULL,
+  `ipaddr` varchar(32) NOT NULL DEFAULT '',
+  `method` char(32) NOT NULL DEFAULT '',
+  `status` int(8) NOT NULL,
+  `insert_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-11  2:56:04
---
+++ homework/day11-20201205/GO3035-beijing-songyunfei/fileserver/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fileserver/process"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	dbtype := "mysql"
+	dsn := "root:admin@ADMIN123456@tcp(192.168.10.128:3306)/log_analysis?parseTime=true&loc=Local&charset=utf8mb4"
+	err := process.InitDB(dbtype,dsn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		err := process.CloseDb()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+	addr := "0.0.0.0:8888"
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/getdata", dataapi)
+	http.HandleFunc("/show", showweb)
+	fmt.Println("启动完成")
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tpl, err := template.ParseFiles("./template/upload.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		Fdata := process.Getfile()
+		err = tpl.ExecuteTemplate(w, "upload.html",Fdata)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		err := r.ParseMultipartForm(1024 * 1024)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer func() {
+			_ = file.Close()
+		}()
+		id,err := process.Filemeta(handler.Filename,handler.Size)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		rder := bufio.NewReader(file)
+		for {
+			data,err:= rder.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				fmt.Println(err)
+			}
+			if err := process.Loginsert(id,data); err != nil {
+				fmt.Println(err)
+			}
+
+		}
+
+	}
+}
+
+func dataapi(w http.ResponseWriter, r *http.Request)  {
+	vars := r.URL.Query()
+	fid := vars.Get("fileid")
+	if fid == "" {
+		rs := strings.NewReader("Id 不能为空")
+		_,_ = io.Copy(w,rs)
+		return
+	}
+	id,err := strconv.ParseInt(fid,10,64)
+	if err != nil {
+		rs := strings.NewReader("非法Id")
+		_,_ = io.Copy(w,rs)
+		return
+	}
+	tdata := process.Readformdb(id)
+	if b,err := json.Marshal(tdata);err == nil{
+		rs := bytes.NewReader(b)
+		_,_ = io.Copy(w,rs)
+		return
+	}
+	fmt.Println(err)
+	return
+
+}
+
+func showweb(w http.ResponseWriter, r *http.Request)  {
+	vars := r.URL.Query()
+	fid := vars.Get("fileid")
+	if fid == "" {
+		rs := strings.NewReader("Id 不能为空")
+		_,_ = io.Copy(w,rs)
+		return
+	}
+	id,err := strconv.ParseInt(fid,10,64)
+	if err != nil {
+		rs := strings.NewReader("非法Id")
+		_,_ = io.Copy(w,rs)
+		return
+	}
+	tpl, err := template.ParseFiles("./template/bar.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	type sdata struct {
+		Fid string
+		Ipdata []map[string]string
+	}
+	var data sdata
+	data.Fid = fid
+	data.Ipdata = process.Readformdb(id).Databar
+	if err = tpl.ExecuteTemplate(w, "bar.html",data); err != nil{
+		fmt.Println(err)
+		return
+	}
+}
