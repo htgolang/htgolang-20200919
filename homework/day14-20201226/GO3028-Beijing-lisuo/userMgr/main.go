0865e455e17fb3e1a08efb626bdd9daa90bc19ca
root <root@suosuoli.cn>
2021-01-19 19:12:58 +0800

    update home.html

--- homework/day14-20201226/GO3028-Beijing-lisuo/userMgr/main.go
+++ homework/day14-20201226/GO3028-Beijing-lisuo/userMgr/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+"fmt"
 	"userMgr/logger"
 	_ "userMgr/models"
 	_ "userMgr/routers"
@@ -11,30 +12,8 @@ import (
 
 func main() {
 	logger.InitLogger()
-	//models.Init()
-	//var driverName = "mysql"
-	//var dsn = "web:web@tcp(127.0.0.1:3306)/user?parseTime=true&loc=Local&charset=utf8mb4"
-	//if err := models.InitDB(driverName, dsn); err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//defer models.CloseDB()
-	//// test
-	//o := orm.NewOrm()
-	//fmt.Printf("orm: %#v\n", o)
-	//qs := o.QueryTable("user")
-	//fmt.Printf("qs: %#v\n", qs)
-	//fmt.Println(qs.Count())
-	//var users = []*models.User{}
-	//n, err := qs.All(&users)
-	//fmt.Printf("query result: %#v, err: %#v\n", n, err)
-	//for k, v := range users {
-	//	fmt.Printf("k: %#v, v: %#v", k, v)
-	//}
-	////
-	//fmt.Printf("dsn: %#v\n", beego.AppConfig.String("db::dsn"))
+	fmt.Printf("dsn: %#v\n", beego.AppConfig.String("db::dsn"))
 	services.IfAdmin()
-
 	logger.Logger.Debug("App running...")
 	beego.Run()
 }
